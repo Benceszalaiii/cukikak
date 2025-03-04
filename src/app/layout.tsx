d406f68f737c66470121fde38347c5ff79dc21b8
Benceszalaiii <szalai.bence.bendeguz@students.jedlik.eu>
2025-03-04 20:38:34 +0100

    OG image changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,31 @@
-
+import { Toaster } from "@/components/ui/sonner";
 import { cx } from "class-variance-authority";
 import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
+import Link from "next/link";
 import { caveat, geistMono, geistSans, inter, netflix, sfpro } from "./fonts";
 import "./globals.css";
-import { Toaster } from "@/components/ui/sonner";
-import Link from "next/link";
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | 11 Corleone",
-    default: "11 Corleone",
+    template: "%s | 11.Corleone",
+    default: "11.Corleone",
   },
-  description: "A 11C osztály kampányoldala",
+  openGraph: {
+    type: "website",
+    locale: "hu_HU",
+    url: "https://11c.vercel.app",
+  },
+  authors: {
+    name: "Szalai Bence",
+    url: "https://www.benceszalai.me",
+  },
+  twitter: {
+    images: ["https://11c.vercel.app/opengraph-image.png"],
+  },
+  category: "education",
+  publisher: "Szalai Bence",
+  description: "11.Corleone - Itt mindenki hülye? Itt? Mindenki. \n Győri SZC Jedlik Ányos Gépipari és Informatikai Technikum 11.C osztályának weboldala.",
 };
 
 export default function RootLayout({
@@ -46,7 +59,21 @@ export default function RootLayout({
           <main className="min-h-screen w-full bg-black selection:bg-red-600 selection:text-black">
             {children}
           </main>
-          <footer className="bg-black px-4 text-center flex flex-col items-center justify-center font-geistmono tracking-wider gap-2 py-4 border-t"><p>Az oldalt készítette <Link draggable={false} className="underline cursor-pointer underline-offset-2 font-semibold font-caveat tracking-widest" href={"https://www.benceszalai.me"}>Szalai Bence</Link></p><p className="text-sm text-gray-100">Tech stack: <span>Next.js, React, Prisma, TailwindCSS</span></p></footer>
+          <footer className="bg-black px-4 text-center flex flex-col items-center justify-center font-geistmono tracking-wider gap-2 py-4 border-t">
+            <p>
+              Az oldalt készítette{" "}
+              <Link
+                draggable={false}
+                className="underline cursor-pointer underline-offset-2 font-semibold font-caveat tracking-widest"
+                href={"https://www.benceszalai.me"}
+              >
+                Szalai Bence
+              </Link>
+            </p>
+            <p className="text-sm text-gray-100">
+              Tech stack: <span>Next.js, React, Prisma, TailwindCSS</span>
+            </p>
+          </footer>
         </ThemeProvider>
       </body>
     </html>
